67e2d4082050b2badeae2e0db9902e6072bfbda1
Praveen <praveen.jirra@gmail.com>
2019-11-26 12:34:49 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,21 @@
+#!/bin/sh
+
+# Make sure to:
+# 1) Name this file `backup.sh` and place it in /home/ubuntu
+# 2) Run sudo apt-get install awscli to install the AWSCLI
+# 3) Run aws configure (enter s3-authorized IAM user and specify region)
+# 4) Fill in DB host + name
+# 5) Create S3 bucket for the backups and fill it in below (set a lifecycle rule to expire files older than X days in the bucket)
+# 6) Run sudo mkdir /data/tmp
+# 7) Run sudo chmod 777 /data/tmp/
+# 8) Run chmod +x backup.sh
+# 9) Test it out via ./backup.sh
+# 10) Set up a daily backup at midnight via `crontab -e`:
+#    0 0 * * * /home/ubuntu/backup.sh > /home/ubuntu/backup.log 2>&1
+# 
+
 # DB host (secondary preferred as to avoid impacting primary performance)
+
 HOST=localhost
 PORT=27017
 
